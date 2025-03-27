ce6f76f0760bb2c9afe06de854ee071dd9514fbe
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-03-27 18:00:47 +0100

    Neovim config update: 2025-03-27 18:00:47

--- lua/sync.lua
+++ lua/sync.lua
@@ -30,13 +30,15 @@ local function commit_and_push_config_changes()
   add_handle:close()
 
   -- Commit changes with simple message
-  local commit_command = 'git commit -m From_sync.lua'
-  local commit_handle = io.popen(commit_command .. ' 2>&1') -- Capture stderr
+  local timestamp = os.date '%Y-%m-%d %H:%M:%S'
+  local message = 'Neovim config update: ' .. timestamp
+  local commit_command = 'git commit -m "' .. message .. '"'
+  local commit_handle = io.popen(commit_command .. ' 2>&1')
   local commit_output = commit_handle:read '*a'
-  local commit_status = commit_handle:close()
+  local commit_success = commit_handle:close()
 
-  if commit_status ~= 0 then
-    vim.notify(tostring(commit_status) .. 'Failed to commit changes:\n' .. commit_output, vim.log.levels.ERROR)
+  if not commit_success then
+    vim.notify('Failed to commit changes:\n' .. commit_output, vim.log.levels.ERROR)
     vim.fn.chdir(current_dir)
     return
   end
@@ -44,8 +46,9 @@ local function commit_and_push_config_changes()
   -- Push changes
   local push_handle = io.popen 'git push 2>&1'
   local push_output = push_handle:read '*a'
-  local push_status = push_handle:close()
-  if push_status ~= 0 then
+  local push_success = push_handle:close()
+
+  if not push_success then
     vim.notify('Failed to push changes:\n' .. push_output, vim.log.levels.ERROR)
     vim.fn.chdir(current_dir)
     return
@@ -56,3 +59,4 @@ local function commit_and_push_config_changes()
 end
 
 vim.api.nvim_create_user_command('ConfigPush', commit_and_push_config_changes, {})
+vim.keymap.set('n', '<leader>gc', ':ConfigPush<CR>')
