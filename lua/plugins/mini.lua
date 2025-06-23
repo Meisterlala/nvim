623fd7a34af6343ba4ff317dc115f699342eeba4
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-06-23 21:18:35 +0200

    2025-06-23-21:18:35

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,35 @@
+local function get_git_nvim_status()
+  local SPECIFIC_REPO_PATH = vim.fn.stdpath 'config'
+  -- Check if the path exists and is a directory
+  if vim.fn.isdirectory(SPECIFIC_REPO_PATH) == 0 then
+    return 'Repo Not Found!'
+  end
+
+  -- Get current branch
+  local branch_cmd = string.format('git -C %s rev-parse --abbrev-ref HEAD', SPECIFIC_REPO_PATH)
+  local branch = vim.fn.system(branch_cmd):gsub('\n', '')
+
+  if branch == '' then
+    return 'Not a Git Repo!'
+  end
+
+  -- Check for uncommitted changes (M = modified, A = added, D = deleted, etc.)
+  local status_cmd = string.format('git -C %s status --porcelain', SPECIFIC_REPO_PATH)
+  local status = vim.fn.system(status_cmd)
+  local has_changes = string.len(status) > 0
+
+  -- Format output
+  local output = '⚡ nvim config updated'
+  if branch ~= 'master' and branch ~= 'main' then
+    output = output .. ' (' .. branch .. ')'
+  end
+
+  if has_changes then
+    return output
+  end
+  return ''
+end
+
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
@@ -21,7 +53,33 @@ return { -- Collection of various small independent plugins/modules
     --  and try some other statusline plugin
     local statusline = require 'mini.statusline'
     -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+    statusline.setup {
+      content = {
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+          local git = MiniStatusline.section_git { trunc_width = 40 }
+          local diff = MiniStatusline.section_diff { trunc_width = 75 }
+          local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+          local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+          local filename = MiniStatusline.section_filename { trunc_width = 140 }
+          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+          local location = MiniStatusline.section_location { trunc_width = 75 }
+          local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+          return MiniStatusline.combine_groups {
+            { hl = mode_hl, strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+            '%<', -- Mark general truncate point
+            { hl = 'MiniStatuslineFilename', strings = { filename } },
+            '%=', -- End left alignment
+            { hl = 'gitstatus', strings = { get_git_nvim_status() } },
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          }
+        end,
+      },
+      use_icons = vim.g.have_nerd_font,
+    }
 
     -- You can configure sections in the statusline by overriding their
     -- default behavior. For example, here we set the section for
