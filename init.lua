d788fab6a8880608303b65910b6a2ec595d5c5be
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-07-08 19:17:04 +0200

    2025-07-08-19:17:04

--- init.lua
+++ init.lua
@@ -37,117 +37,110 @@
 --            Meisterlala's  NeoVim config
 --]
 
--- Set <space> as the leader key
--- See `:help mapleader`
+-- [[ Leader Key Configuration ]]
+-- Set <space> as the leader key for custom shortcuts throughout Neovim.
+-- See :help mapleader and :help maplocalleader for details.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to english
+-- [[ Language and Encoding ]]
+-- Set local language to English for consistent messages and sort order.
+-- See :help :language
 vim.cmd 'language C'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
+-- Used to determine icon style in plugin UIs (see below)
 vim.g.have_nerd_font = false
 
--- Make line numbers default
-vim.opt.number = true
--- vim.opt.relativenumber = true
+-- [[ Editor and UI Options ]]
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+-- Line Numbers
+vim.opt.number = true -- Show absolute line numbers
+-- vim.opt.relativenumber = true          -- (Optional) Show relative line numbers for navigation
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+-- Window Splitting
+vim.opt.splitright = true -- Vertical splits open to the right
+vim.opt.splitbelow = true -- Horizontal splits open below
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
---vim.schedule(function()
---  vim.opt.clipboard = 'unnamedplus'
---end)
+-- Mouse and Cursor
+vim.opt.mouse = 'a' -- Enable mouse in all modes
+vim.opt.cursorline = true -- Highlight the line with the cursor
 
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
+-- Display Options
+vim.opt.signcolumn = 'yes' -- Always show signcolumn to prevent text shifting
+vim.opt.list = true -- Show tabs, trailing spaces, etc.
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+-- Search
+vim.opt.ignorecase = true -- Case-insensitive search...
+vim.opt.smartcase = true -- ...unless pattern has uppercase or \C
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
+-- Indentation and Wrapping
+vim.opt.breakindent = true -- Wrapped lines get extra indentation
 
--- Minimal number of screen lines to keep above and below the cursor.
+-- Undo and File Handling
+vim.opt.undofile = true -- Save undo history to disk
+vim.opt.confirm = true -- Prompt to save changes instead of failing
+
+-- Responsiveness
+vim.opt.updatetime = 250 -- Faster CursorHold update
+vim.opt.timeoutlen = 300 -- Faster mapped sequence timeout
+
+-- Substitute Preview
+vim.opt.inccommand = 'split' -- Live preview for :substitute
+
+-- Keep lines above/below cursor
 vim.opt.scrolloff = 10
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = true
+-- Don't show mode (already in statusline)
+vim.opt.showmode = false
+
+-- [[ Clipboard Integration (optional) ]]
+--vim.schedule(function()
+--  vim.opt.clipboard = 'unnamedplus'     -- Uncomment to use the system clipboard
+--end)
 
 -- [[ Basic Keymaps ]]
+--
+-- Most keymaps are set using vim.keymap.set({mode}, {lhs}, {rhs}, {opts})
+-- See :help vim.keymap.set
 
--- Set up config sync
+-- Set up config sync via Lua module (see lua/sync.lua)
 require 'sync'
 vim.keymap.set('n', '<leader>u', ':ConfigPush<CR>', { desc = '[U]pload the nvim config to GitHub' })
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
+-- Pressing <Esc> in normal mode will clear search highlighting
+-- See :help hlsearch
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+-- <leader>q: Open list of diagnostics in location list (see :help diagnostic)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
+-- Easier shortcut to exit terminal mode: double ESC (instead of <C-\><C-n>)
+-- Helpful for terminal users new to Neovim
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
+-- TIP: Uncomment to disable arrow keys in normal mode, encouraging hjkl movement
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
+-- Keybinds for fast window navigation with Ctrl+Arrow keys
+-- See :help wincmd for details on window commands
 vim.keymap.set('n', '<C-left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+--
+-- See :help lua-guide-autocommands for details on autocommand usage.
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+-- Highlight yanked (copied) text briefly for visual feedback
+-- Try it by selecting text in normal mode and pressing "yap"
+-- See :help vim.highlight.on_yank()
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -156,9 +149,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- [[ Install and Bootstrap `lazy.nvim` Plugin Manager ]]
+--
+-- This block ensures lazy.nvim is installed to manage all other plugins.
+-- See :help lazy.nvim.txt or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -169,15 +163,18 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
+-- [[ Configure and Load Plugins with lazy.nvim ]]
+--
+-- All actual plugin specs are imported from lua/plugins/
+-- Icons are set based on Nerd Font status above for best compatibility.
 require('lazy').setup({
   { import = 'plugins' },
 }, {
   checker = {
-    enabled = true,
+    enabled = true, -- Enable automatic plugin update checks
   },
   change_detection = {
-    notify = false,
+    notify = false, -- Don't show notification when plugin settings change
   },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -200,5 +197,7 @@ require('lazy').setup({
   },
 })
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- The line beneath this is called `modeline`.
+-- It helps some editors detect and respect this file's indentation settings.
+-- See :help modeline
 -- vim: ts=2 sts=2 sw=2 et
