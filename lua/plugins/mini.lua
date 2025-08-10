d3ce3f8f7ab40cef3da74f1f744e635ed9d8ee58
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-10 19:52:29 +0200

    2025-08-10-19:52:29

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -22,6 +22,40 @@ local function sync_git_status()
   return sync_mod.get_nvim_config_git_status() or ''
 end
 
+-- Really hacky to have this here
+local function chezmoi_file()
+  local current_file = vim.fn.expand '%:p' -- Full path of the current file
+  local home_dir = vim.env.HOME -- Get the home directory
+  local chezmoi_dir = home_dir .. '/.local/share/chezmoi'
+
+  -- Skip if editing the source file in the chezmoi directory
+  if current_file:sub(1, #chezmoi_dir) == chezmoi_dir then
+    return ''
+  end
+
+  local ok, chezmoi = pcall(require, 'chezmoi.commands')
+  if not ok then
+    return ''
+  end
+
+  local managed_files = chezmoi.list()
+  for _, file in ipairs(managed_files) do
+    local full_managed_path = home_dir .. '/' .. file
+    if full_managed_path == current_file then
+      local edit_watch = function()
+        vim.notify 'You opened a file that is managed by chezmoi'
+        require('chezmoi.commands').edit {
+          targets = { current_file },
+          args = { '--watch', '--appyl' },
+        }
+      end
+      vim.schedule(edit_watch)
+      return 'Chezmoi File'
+    end
+  end
+  return ''
+end
+
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
@@ -61,6 +95,7 @@ return { -- Collection of various small independent plugins/modules
           local location = MiniStatusline.section_location { trunc_width = 75 }
           local search = MiniStatusline.section_searchcount { trunc_width = 75 }
           local git_status = sync_git_status()
+          local chezmoi_status = chezmoi_file()
 
           return MiniStatusline.combine_groups {
             { hl = mode_hl, strings = { mode } },
@@ -68,7 +103,7 @@ return { -- Collection of various small independent plugins/modules
             '%<', -- Mark general truncate point
             { hl = 'MiniStatuslineFilename', strings = { filename } },
             '%=', -- End left alignment
-            { hl = 'gitstatus', strings = { git_status } },
+            { hl = 'gitstatus', strings = { chezmoi_status, git_status } },
             { hl = 'MiniStatuslineFileinfo', strings = { lazy_updates_status(), fileinfo } },
             { hl = mode_hl, strings = { search, location } },
           }
