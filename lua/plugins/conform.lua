b93f10ad155b222e1f8b4e4889da9d870e566513
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-11 18:06:25 +0200

    2025-08-11-18:06:25

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -30,6 +30,8 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      json = { 'prettierd' },
+      jsonc = { 'prettierd' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
