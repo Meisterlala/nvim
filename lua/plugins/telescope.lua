90315e5064a1a6e1b9ae2c6e3a88a8bd777ced0e
Robin Rottstädt <r.rottstaedt@sv-gruppe.de>
2025-08-12 16:58:58 +0200

    2025-08-12-16:58:58

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,16 +5,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter' },
     'nvim-lua/plenary.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
-      'nvim-telescope/telescope-fzf-native.nvim',
-
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
-      build = 'make',
-
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
-    },
     { 'nvim-telescope/telescope-ui-select.nvim' },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
