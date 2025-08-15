184466762b19e97740caa2594a4ca0f8f760ea0f
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-15 18:22:34 +0200

    2025-08-15-18:22:34

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -3,15 +3,6 @@ return {
   priority = 1000, -- Make sure to load this before all the other start plugins.
   name = 'catppuccin',
   config = function()
-    -- Setup notify background
-    vim.api.nvim_create_autocmd('ColorScheme', {
-      callback = function()
-        vim.api.nvim_set_hl(0, 'NotifyINFOBody', { bg = 'NONE' })
-        vim.api.nvim_set_hl(0, 'NotifyINFOBorder', { bg = 'NONE' })
-        vim.api.nvim_set_hl(0, 'NotifyINFOTitle', { bg = 'NONE' })
-      end,
-    })
-
     ---@diagnostic disable-next-line: missing-fields
     require('catppuccin').setup {
       flavour = 'mocha',
