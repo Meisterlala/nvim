6b7670ed467335d3625f9f30993c78d145e32c6a
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-15 18:59:54 +0200

    2025-08-15-18:59:54

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 15
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<leader>t]],
+      shade_terminals = false,
+    },
+    keys = {
+      { '<leader>t', nil, desc = 'Open [T]erminal' },
+    },
+  },
+}
