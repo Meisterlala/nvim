1557fa4b302bd14038a24292f52a62347d1afbc3
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-18 23:10:59 +0200

    2025-08-18-23:10:59

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -47,6 +47,33 @@ return {
         -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
         auto_refresh = true,
 
+        -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
+        integrations = {
+          -- If enabled, use telescope for menu selection rather than vim.ui.select.
+          -- Allows multi-select and some things that vim.ui.select doesn't.
+          telescope = true,
+          -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
+          -- The diffview integration enables the diff popup.
+          --
+          -- Requires you to have `sindrets/diffview.nvim` installed.
+          diffview = true,
+
+          -- If enabled, uses fzf-lua for menu selection. If the telescope integration
+          -- is also selected then telescope is used instead
+          -- Requires you to have `ibhagwan/fzf-lua` installed.
+          fzf_lua = false,
+
+          -- If enabled, uses mini.pick for menu selection. If the telescope integration
+          -- is also selected then telescope is used instead
+          -- Requires you to have `echasnovski/mini.pick` installed.
+          mini_pick = false,
+
+          -- If enabled, uses snacks.picker for menu selection. If the telescope integration
+          -- is also selected then telescope is used instead
+          -- Requires you to have `folke/snacks.nvim` installed.
+          snacks = false,
+        },
+
         mappings = {
           commit_editor = {
             ['q'] = 'Close',
