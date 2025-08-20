6ee89f9e07a349d36b9f4e10ac4fc13160ac790a
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-20 16:57:49 +0200

    2025-08-20-16:57:49

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,6 +6,10 @@ local function toggle_copilot()
     vim.notify('Copilot enabled', vim.log.levels.INFO, { title = 'Copilot' })
   else
     require('copilot.command').disable()
+
+    -- Rebind <M-y> to <C-y>, so that auto-complete falls back to nvim-cmp
+    vim.api.nvim_set_keymap('i', '<M-y>', '<C-y>', { silent = true })
+
     vim.notify('Copilot disabled', vim.log.levels.INFO, { title = 'Copilot' })
   end
 end
@@ -36,26 +40,18 @@ return {
       }
 
       -- Hide Copilot suggestions when the completion menu is open
+      --[[
       local cmp_ok, cmp = pcall(require, 'cmp')
       if cmp_ok then
         cmp.event:on('menu_opened', function()
-          -- vim.b.copilot_suggestion_hidden = true
+          vim.b.copilot_suggestion_hidden = true
         end)
 
         cmp.event:on('menu_closed', function()
-          -- vim.b.copilot_suggestion_hidden = false
+          vim.b.copilot_suggestion_hidden = false
         end)
       end
-
-      -- Rebind <M-y> to <C-y>, if there are no suggestions
-      local copilot_suggestion = require 'copilot.suggestion'
-      vim.keymap.set('i', '<M-y>', function()
-        if copilot_suggestion.is_visible() then
-          copilot_suggestion.accept()
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-y>', true, false, true), 'n', true)
-        end
-      end, { desc = 'Accept Copilot suggestion' })
+      --]]
     end,
     keys = {
       {
