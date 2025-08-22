a539987b394e1a9ff3d8d39c6982172fa789d981
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-22 19:13:58 +0200

    2025-08-22-19:13:58

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -39,10 +39,8 @@ return {
         width = 85,
         preset = {
           keys = {
-            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = ' ', key = 't', desc = 'Telescope Find Files', action = ':Telescope find_files' },
+            { icon = ' ', key = 'f', desc = 'Find File', action = ':Telescope find_files' },
             { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
             { icon = ' ', key = 's', desc = 'Restore Session', action = '<cmd>SessionSearch<CR>' },
@@ -58,8 +56,8 @@ return {
             text = { { string.format('v%d.%d.%d', vim.version().major, vim.version().minor, vim.version().patch), align = 'center' } },
           },
           { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', limit = 8, indent = 2, padding = 1 },
           { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
           { section = 'startup' },
         },
       },
