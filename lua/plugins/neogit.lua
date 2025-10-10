2fcf16cc743d800cfcff400f62405e48c4eeb483
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-10-10 22:58:24 +0200

    2025-10-10-22:58:24

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -10,7 +10,7 @@ return {
           -- the right pane. You can change this to 'diff1' to show only the current file's diff.
           view = {
             default = {
-              layout = 'diff3_horizomtal',
+              layout = 'diff2_horizontal',
             },
           },
         },
@@ -43,12 +43,6 @@ return {
         -- Show message with spinning animation when a git command is running.
         process_spinner = true,
         -- Used to generate URL's for branch popup action "pull request".
-        git_services = {
-          ['github.com'] = 'https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1',
-          ['bitbucket.org'] = 'https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1',
-          ['gitlab.com'] = 'https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}',
-          ['azure.com'] = 'https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}',
-        },
         -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
         -- sorter instead. By default, this function returns `nil`.
         -- telescope_sorter = function()
