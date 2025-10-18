8e7345e99235c8884d0f1817981e12386f2e192e
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-10-18 15:14:39 +0200

    2025-10-18-15:14:39

--- lua/plugins/k8s-crd.lua
+++ lua/plugins/k8s-crd.lua
@@ -3,7 +3,7 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   ft = 'yaml',
   dependencies = { 'neovim/nvim-lspconfig' },
-  setup = function()
+  config = function()
     require('nvim-k8s-crd').setup {
       cache_dir = vim.fs.joinpath(vim.fn.stdpath 'data', 'k8s-crd-cache'),
       k8s = {
