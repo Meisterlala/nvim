13467a065df3cf87502b9c3fcaa21afd71ec1d8c
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-11-07 21:32:18 +0100

    2025-11-07-21:32:18

--- init.lua
+++ init.lua
@@ -156,7 +156,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode with Esc
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Close and Save with leader
 vim.keymap.set('n', '<leader>m', '<cmd>update<CR>', { desc = 'Write File', silent = true })
