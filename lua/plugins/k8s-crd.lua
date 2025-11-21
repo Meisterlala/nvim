5cdb66caee32416373e68631b9826239f802a228
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-11-21 10:45:12 +0100

    2025-11-21-10:45:12

--- lua/plugins/k8s-crd.lua
+++ lua/plugins/k8s-crd.lua
@@ -1,10 +1,13 @@
 --- @type LazySpec | LazySpec[]
 return {
   'anasinnyk/nvim-k8s-crd',
-  event = { 'BufReadPre', 'BufNewFile' },
-  ft = 'yaml',
+  ft = { 'yaml' },
   dependencies = { 'neovim/nvim-lspconfig' },
   config = function()
+    -- Check if kubectl is installed, if not, skip the setup
+    if vim.fn.executable 'kubectl' == 0 then
+      return
+    end
     require('nvim-k8s-crd').setup {
       cache_dir = vim.fs.joinpath(vim.fn.stdpath 'data', 'k8s-crd-cache'),
       k8s = {
