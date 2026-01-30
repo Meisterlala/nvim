20175fa8ebfdc5d875a22c3e6010d884e9ec7009
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2026-01-30 22:53:21 +0100

    2026-01-30-22:53:21

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -1,9 +1,19 @@
 return {
   settings = {
     yaml = {
-      schemas = {
-        sdf = '.yaml',
+      schemaStore = {
+        enable = false,
+        url = '',
       },
+      -- parse Kubernetes CRDs automatically and download them from the CRD store.
+      kubernetesCRDStore = {
+        enable = true,
+      },
+      -- Can never be emtpy, because of a nvim-k8s-crd bug
+      schemas = {
+        ['https://json.schemastore.org/kustomization.json'] = '**/kustomization.yaml',
+      },
+      validate = true,
     },
   },
 }
