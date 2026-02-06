5a71d014bc0bc8e18020791c36a5b24ca4200e44
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2026-02-06 10:39:11 +0100

    fix c#

---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,10 @@
+--- @type LazySpec
+return {
+  'seblyng/roslyn.nvim',
+  ft = { 'cs', 'vb' },
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+}
