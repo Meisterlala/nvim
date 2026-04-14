3c0a383a65e04ed3fee7f27db077455304820536
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2026-04-14 13:50:28 +0200

    feat(plugins): remove img-clip.nvim integration

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -279,21 +279,6 @@ return {
     -- Copilot integration
     'zbirenbaum/copilot.lua',
 
-    -- Image pasting support
-    {
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          use_absolute_path = true,
-        },
-      },
-    },
 
     -- Markdown rendering for Avante
     {
