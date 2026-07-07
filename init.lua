952ea3fb46316ff49536cbafebc2309baad3d183
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2026-07-07 13:40:58 +0200

    fix(highlight): update yank highlight function to use new API method

--- init.lua
+++ init.lua
@@ -251,13 +251,11 @@ vim.api.nvim_create_autocmd('TabClosed', {
 })
 
 -- Highlight yanked (copied) text briefly for visual feedback
--- Try it by selecting text in normal mode and pressing "yap"
--- See :help vim.highlight.on_yank()
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.hl_op()
   end,
 })
 
